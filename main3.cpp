e8453e9cf34024bc9ca7901733edaa474091bf93
David Kalcic <kalcicd@oregonstate.edu>
2021-03-02 16:41:18 -0800

    yeet

--- main3.cpp
+++ main3.cpp
@@ -9,28 +9,24 @@ using namespace std;
 
 #define BLOCK_SIZE 11
 
-struct EmpBlock
-{
+struct EmpBlock{
     int eid;
     string ename;
     int age;
     double salary;
 
-    bool operator()(EmpBlock const & a, EmpBlock const & b) const
-    {
+    bool operator()(EmpBlock const& a, EmpBlock const& b) const{
         return a.eid < b.eid;
     }
 };
 
-struct DeptBlock
-{
+struct DeptBlock{
     int did;
     string dname;
     double budget;
     int managerid;
 
-    bool operator()(DeptBlock const & a, DeptBlock const & b) const
-    {
+    bool operator()(DeptBlock const& a, DeptBlock const& b) const{
         return a.managerid < b.managerid;
     }
 };
@@ -38,13 +34,11 @@ struct DeptBlock
 // Grab a single block from the emp.csv file, in theory if a block was larger than
 // one tuple, this function would return an array of EmpBlocks and the entire if
 // and else statement would be wrapped in a loop for x times based on block size
-EmpBlock grabEmp(fstream &empin)
-{
+EmpBlock grabEmp(fstream& empin){
     string line, word;
     EmpBlock emp;
     // grab entire line
-    if (getline(empin, line, '\n'))
-    {
+    if(getline(empin, line, '\n')){
         // turn line into a stream
         stringstream s(line);
         // gets everything in stream up to comma
@@ -57,9 +51,7 @@ EmpBlock grabEmp(fstream &empin)
         getline(s, word, ',');
         emp.salary = stod(word);
         return emp;
-    }
-    else
-    {
+    } else{
         emp.eid = -1;
         return emp;
     }
@@ -68,12 +60,10 @@ EmpBlock grabEmp(fstream &empin)
 // Grab a single block from the dept.csv file, in theory if a block was larger than
 // one tuple, this function would return an array of DeptBlocks and the entire if
 // and else statement would be wrapped in a loop for x times based on block size
-DeptBlock grabDept(fstream &deptin)
-{
+DeptBlock grabDept(fstream& deptin){
     string line, word;
     DeptBlock dept;
-    if (getline(deptin, line, '\n'))
-    {
+    if(getline(deptin, line, '\n')){
         stringstream s(line);
         getline(s, word, ',');
         dept.did = stoi(word);
@@ -84,23 +74,20 @@ DeptBlock grabDept(fstream &deptin)
         getline(s, word, ',');
         dept.managerid = stoi(word);
         return dept;
-    }
-    else
-    {
+    } else{
         dept.did = -1;
         return dept;
     }
 }
 
 //Print out the attributes from emp and dept when a join condition is met
-void printJoin(EmpBlock emp, DeptBlock dept, fstream &fout)
-{
+void printJoin(EmpBlock emp, DeptBlock dept, fstream& fout){
     fout << emp.eid << ',' << emp.ename << ',' << emp.age << ','
          << emp.salary << ',' << dept.did << ',' << dept.dname << ','
          << dept.budget << "\n";
 }
 
-void mergeJoin(vector< vector< EmpBlock > > empList, vector< vector< DeptBlock > > deptList) {
+void mergeJoin(vector<vector<EmpBlock> > empList, vector<vector<DeptBlock> > deptList){
     fstream joinout;
     joinout.open("Join.csv", ios::out | ios::app);
 
@@ -121,20 +108,20 @@ void mergeJoin(vector< vector< EmpBlock > > empList, vector< vector< DeptBlock >
     joinout.close();
 }
 
-vector< vector< EmpBlock > > sortAndStoreEmps(vector< vector< EmpBlock > > empList) {
+vector<vector<EmpBlock> > sortAndStoreEmps(vector<vector<EmpBlock> > empList){
     fstream empin;
     vector<EmpBlock> tempEmp = {};
     vector<EmpBlock> smallTempEmp = {};
 
     empin.open("Emp.csv", ios::in);
-    while (true) {
+    while(true){
         // FOR BLOCK IN RELATION EMP
         // grabs a block
         EmpBlock empBlock = grabEmp(empin);
         // checks if filestream is empty
-        if (empBlock.eid == -1) {
+        if(empBlock.eid == -1){
             break;
-        } else {
+        } else{
             tempEmp.push_back(empBlock);
         }
     }
@@ -144,12 +131,12 @@ vector< vector< EmpBlock > > sortAndStoreEmps(vector< vector< EmpBlock > > empLi
     int runs = 0;
 
     // split employees into groups of 11 per memory constraints
-    for(int ii = 0; ii < tempEmp.size() % BLOCK_SIZE; ii ++) {
-        for(int jj = 0; jj < BLOCK_SIZE; jj ++) {
-            runs ++;
-            if(runs < tempEmp.size()) {
+    for(int ii = 0; ii < tempEmp.size() % BLOCK_SIZE; ii++){
+        for(int jj = 0; jj < BLOCK_SIZE; jj++){
+            runs++;
+            if(runs < tempEmp.size()){
                 smallTempEmp.push_back(tempEmp[jj + ii * BLOCK_SIZE]);
-            } else {
+            } else{
                 break;
             }
         }
@@ -161,22 +148,21 @@ vector< vector< EmpBlock > > sortAndStoreEmps(vector< vector< EmpBlock > > empLi
 
 }
 
-vector< vector< DeptBlock > > sortAndStoreDepts(vector< vector< DeptBlock > > deptList) {
-    
+vector<vector<DeptBlock> > sortAndStoreDepts(vector<vector<DeptBlock> > deptList){
     cout << "i made it" << endl;
     fstream deptin;
     vector<DeptBlock> tempDept = {};
     vector<DeptBlock> smallTempDept = {};
 
     deptin.open("Dept.csv", ios::in);
-    while (true) {
+    while(true){
         // FOR BLOCK IN RELATION DEPT
         // grabs a block
         DeptBlock deptBlock = grabDept(deptin);
         // checks if filestream is empty
-        if (deptBlock.did == -1) {
+        if(deptBlock.did == -1){
             break;
-        } else {
+        } else{
             tempDept.push_back(deptBlock);
         }
     }
@@ -188,12 +174,12 @@ vector< vector< DeptBlock > > sortAndStoreDepts(vector< vector< DeptBlock > > de
 
     // split departments into groups of 11 per memory constraints
 
-    for(int ii = 0; ii < tempDept.size() % BLOCK_SIZE; ii ++) {
-        for(int jj = 0; jj < BLOCK_SIZE; jj ++) {
-            runs ++;
-            if(runs < tempDept.size()) {
+    for(int ii = 0; ii < tempDept.size() % BLOCK_SIZE; ii++){
+        for(int jj = 0; jj < BLOCK_SIZE; jj++){
+            runs++;
+            if(runs < tempDept.size()){
                 smallTempDept.push_back(tempDept[jj + ii * BLOCK_SIZE]);
-            } else {
+            } else{
                 break;
             }
         }
@@ -203,34 +189,36 @@ vector< vector< DeptBlock > > sortAndStoreDepts(vector< vector< DeptBlock > > de
 
     deptin.close();
     return deptList;
-    
+
 }
 
-int main() {
-    vector< vector< EmpBlock > > empList = sortAndStoreEmps(empList);
-        cout << "swagger man ultimate" << endl;
+int main(){
+    vector<vector<EmpBlock> > empList;
+    empList = sortAndStoreEmps(empList);
+    cout << "swagger man ultimate" << endl;
 
-    vector< vector< DeptBlock > > deptList = sortAndStoreDepts(deptList);
+    vector<vector<DeptBlock> > deptList;
+    deptList = sortAndStoreDepts(deptList);
 
     // flags check when relations are done being read
-    
+
 
     //REMOVE THIS WHEN UR DONE KIDDO
-    for(int ii = 0; ii < empList.size(); ii ++) {
-        for(int jj = 0; jj < empList[ii].size(); jj ++) {
+    for(int ii = 0; ii < empList.size(); ii++){
+        for(int jj = 0; jj < empList[ii].size(); jj++){
             cout << empList[ii][jj].eid << endl;
         }
     }
     //^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
     //REMOVE THIS WHEN UR DONE KIDDO
-    for(int ii = 0; ii < deptList.size(); ii ++) {
-        for(int jj = 0; jj < deptList[ii].size(); jj ++) {
+    for(int ii = 0; ii < deptList.size(); ii++){
+        for(int jj = 0; jj < deptList[ii].size(); jj++){
             cout << deptList[ii][jj].managerid << endl;
         }
     }
 
     mergeJoin(empList, deptList);
-    
+
     return 0;
 }
